9cca6524d16a213b0f7b5081e6586955461442bf
BIGON ALESSIO <9698256@itisrossi.edu.it>
2024-12-07 08:46:58 +0100

    implementazioni varie

--- TGBOT/src/Main.java
+++ TGBOT/src/Main.java
@@ -1,13 +1,16 @@
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        WebScraper ws = new WebScraper();
-        try{
-            List<Recipe> recipes = ws.scrape();
-            recipes.forEach(System.out::println);
-        } catch (IOException e) {
+        String botToken = "7584434037:AAFk0dJk79a5R8Unqn42x1zeG9LD5qh0Aus";
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new TGAPI(botToken));
+            System.out.println("YellowCookBot successfully started!");
+            Thread.currentThread().join();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- TGBOT/src/User.java
+++ TGBOT/src/User.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    public String username;
+    private String password;
+    public List<Recipe> favourites;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        this.favourites = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Recipe> getFavourites() {
+        return favourites;
+    }
+
+    public void setFavourites(List<Recipe> favourites) {
+        this.favourites = favourites;
+    }
 }
